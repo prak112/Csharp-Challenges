dc7c0e695a212f4ac7625a7772123d03290a53c3
PrakGov <55034062+prak112@users.noreply.github.com>
2023-09-10 21:35:52 +0300

    detect consumption, accelerate movement, change status

--- miniGame/miniGame-with-missing-features/Starter/Program.cs
+++ miniGame/miniGame-with-missing-features/Starter/Program.cs
@@ -4,7 +4,14 @@ Random random = new Random();
 Console.CursorVisible = false;
 int height = Console.WindowHeight - 1;
 int width = Console.WindowWidth - 5;
-bool shouldExit = false;
+
+// Game termination deciders
+bool terminalResize = false;
+bool invalidInput = false;
+
+// player state validation
+bool playerEatsFood = false;
+int movementSpeed = 0;
 
 // Console position of the player
 int playerX = 0;
@@ -24,15 +31,27 @@ string player = states[0];
 // Index of the current food
 int food = 0;
 
+
+
 // Initiate Gameplay
 InitializeGame();
 do
 {
-    shouldExit = TerminalResized();
-    Move(shouldExit);
-    ShowFood();
-    ChangePlayer();
-}while (!shouldExit);
+    // verify conditions
+    terminalResize = TerminalResized();
+    invalidInput = NonDirectionalInput();
+    playerEatsFood = ConsumedFood();
+    movementSpeed = AcceleratePlayer();
+
+    // activate methods
+    Move(terminalResize, invalidInput, movementSpeed);
+    if(playerEatsFood == true)
+    {
+        ShowFood();
+        ChangePlayer();
+    }
+}while (!terminalResize || !invalidInput);
+
 
 
 
@@ -48,14 +67,26 @@ void InitializeGame()
 }
 
 // Reads directional input from the Console and moves the player
-void Move(bool shouldExit) 
-{
+void Move(bool terminalResize, bool invalidInput, int movementSpeed) 
+{ 
     // close console if Terminal Resized
-    if(shouldExit == true)
+    if(terminalResize == true)
     {
         TerminateGame();        
     }
+
+    // if user pressed non-directional keys, exit
+    if(invalidInput == true)
+    {
+        TerminateGame();
+    }
     
+    // freeze player if player state is sick
+    if(player == states[2])
+    {
+        FreezePlayer();
+    }
+
     // Keep player position within the bounds of the Terminal window
     int lastX = playerX;
     int lastY = playerY;
@@ -68,14 +99,26 @@ void Move(bool shouldExit)
 		case ConsoleKey.DownArrow: 
             playerY++; 
             break;
-		case ConsoleKey.LeftArrow:  
-            playerX--; 
+		case ConsoleKey.LeftArrow:
+            if (player == states[1])    // player is healthy, hence faster
+            {   
+                playerX -= movementSpeed;
+            }
+            else{
+                playerX--;
+            }
             break;
-		case ConsoleKey.RightArrow: 
-            playerX++; 
+		case ConsoleKey.RightArrow:
+            if (player == states[1])    // player is healthy, hence faster
+            {   
+                playerX += movementSpeed;
+            } 
+            else{
+                playerX++;    
+            }             
             break;
 		case ConsoleKey.Escape:     
-            shouldExit = true; 
+            terminalResize = true; 
             break;
     }
 
@@ -86,6 +129,7 @@ void Move(bool shouldExit)
         Console.Write(" ");
     }
 
+
     // Keep player position within the bounds of the Terminal window
     playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
     playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
@@ -95,16 +139,7 @@ void Move(bool shouldExit)
     Console.Write(player);
 }
 
-// Returns true if the Terminal was resized 
-bool TerminalResized() 
-{
-    // verify height and width
-    if (height!= Console.WindowHeight - 1 || width!= Console.WindowWidth - 5)   // terminal resized
-    {
-        return true;
-    }
-    return false;
-}
+
 
 // Displays random food at a random location
 void ShowFood() 
@@ -136,11 +171,70 @@ void FreezePlayer()
     player = states[0];
 }
 
+#endregion
+
+
+
+#region Helper Methods
+
+// Accelerate the player
+int AcceleratePlayer()
+{
+    if (player == states[1])
+    {
+        movementSpeed = 3;
+    }
+    else
+    {
+        movementSpeed = 0;
+    }
+    return movementSpeed;
+}
+
+
+// Returns true if the player location matches the food location
+bool ConsumedFood() 
+{
+    // confirm player location overlaps food location, hence food consumed
+    if (playerY == foodY && playerX == foodX)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+// Returns true if the Terminal was resized 
+bool TerminalResized() 
+{
+    // verify height and width
+    if (height!= Console.WindowHeight - 1 || width!= Console.WindowWidth - 5)   // terminal resized
+    {
+        return true;
+    }
+    return false;
+}
+
+// Detects non-directional input, i.e., if user pressed other than arrow keys
+bool NonDirectionalInput() 
+{
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+        
+    if (keyInfo.Key == ConsoleKey.UpArrow || 
+        keyInfo.Key == ConsoleKey.DownArrow || 
+        keyInfo.Key == ConsoleKey.LeftArrow || 
+        keyInfo.Key == ConsoleKey.RightArrow) 
+    {
+        return false;
+    }
+    return true;
+}
+
 // Terminate game, if terminal resized or user pressed non-directional keys
 void TerminateGame()
 {
     Console.Clear();    // clear console
-    Console.WriteLine("Console was resized. Program exiting...");
+    Console.WriteLine("Console was resized OR non-directional keys were pressed.\nProgram exiting...");
     Thread.Sleep(2000);  // pause for 2 seconds
     Environment.Exit(0);    // close game
 }
